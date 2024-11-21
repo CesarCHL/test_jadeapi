efc3666d232d90fcece9d6abf8113e279cf644d5
CesarCHL <cesarchavira314@gmail.com>
2024-11-21 15:50:34 -0600

    Modularization

--- test_api.py
+++ test_api.py
@@ -1,20 +1,12 @@
-import requests
-import random
-import pytest
-from dotenv import load_dotenv
-from typing import List, Dict
+# test_api.py
 import os
+import pytest
+from logic import get_next_trip, post_request
+from dotenv import load_dotenv
 
 load_dotenv()
 
-def post_request(url: str, data: Dict):
-  response = requests.post(url, json=data)
-  print(f"Response status code: {response.status_code}")
-  print(f"Response content: {response.json}")
-  assert  response.status_code in [200, 201]
-  return response.json()
-
-# POST addTripsPlanning
+#Post addTripPlanning
 def test_addTrip_Planning():
     url = os.getenv("API_tripPlanning")
     data = { 
@@ -24,89 +16,44 @@ def test_addTrip_Planning():
     }
     post_request(url, data)
 
-# POST addTrips
+#Post addTrip
 def test_addTrip():
     url = os.getenv("API_addTrip")
     data = {
-    "trip": "0987",
-    "planta": "PTA 1 (CD)",
-    "tipoCarga": "CARGA REGULAR",
-    "tipoCaja":"CAJA SECA",
-    "areaCarga": "EXPLANADA/BODEGA",
-    "turno": "2",
-    "tipoTurno":"GRUPO_COM",
-    "carrier": "Carrier 2",
-    "driver": "Driver 2"
-}
+        "trip": get_next_trip(),
+        "planta": "PTA 1 (CD)",
+        "tipoCarga": "CARGA REGULAR",
+        "tipoCaja":"CAJA SECA",
+        "areaCarga": "EXPLANADA/BODEGA",
+        "turno": "2",
+        "tipoTurno":"GRUPO_COM",
+        "carrier": "Carrier 2",
+        "driver": "Driver 2"
+    }
     post_request(url, data)
 
-# POST stagingEvent
+#Post steagingEvent
 def test_stagingEvent():
     url = os.getenv("API_stagingEvent")
     data = {
         "factoryPlant": "Pta 1 (CD)",
         "screenLine": 1,
         "sensors": [
-           {
-             "number": 1,
-             "active": 0
-           },
-           {
-             "number": 2,
-             "active": 0
-           },
-           {
-             "number": 3,
-             "active": 0
-           },
-           {
-             "number": 4,
-             "active": 0
-           },
-           {
-             "number": 5,
-             "active": 0
-           },
-           {
-             "number": 6,
-             "active": 0
-           },
-           {
-             "number": 7,
-             "active": 0
-           },
-           {
-             "number": 8,
-             "active": 0
-           },
-           {
-             "number": 9,
-             "active": 1
-           },
-           {
-             "number": 10,
-             "active": 0
-           },
-           {
-             "number": 11,
-             "active": 0
-           },
-           {
-             "number": 12,
-             "active": 0
-           },
-           {
-             "number": 13,
-             "active": 0
-           },
-           {
-             "number": 14,
-             "active": 0
-           },
-           {
-             "number": 15,
-             "active": 0
-           }
+           {"number": 1, "active": 0},
+           {"number": 2, "active": 0},
+           {"number": 3, "active": 0},
+           {"number": 4, "active": 0},
+           {"number": 5, "active": 0},
+           {"number": 6, "active": 0},
+           {"number": 7, "active": 0},
+           {"number": 8, "active": 0},
+           {"number": 9, "active": 1},
+           {"number": 10, "active": 0},
+           {"number": 11, "active": 0},
+           {"number": 12, "active": 0},
+           {"number": 13, "active": 0},
+           {"number": 14, "active": 0},
+           {"number": 15, "active": 0}
         ]
     }
     post_request(url, data)
