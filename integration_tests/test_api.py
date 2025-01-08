6364a7c219a9d36f89080ac88a49ef3c13984757
CesarCHL <cesarchavira314@gmail.com>
2025-01-08 12:58:23 -0600

    Code reorganization(unfinished)

--- integration_tests/test_api.py
+++ integration_tests/test_api.py
@@ -1,85 +1,76 @@
 # test_api.py
-import os
-from integration_tests.logic import   post_request, update_trip
+import os, time
+from logic import post_request, update_trip
 from dotenv import load_dotenv
-import random
 import pytest
 
 load_dotenv()
 
-#Post addTripPlanning
+@pytest.fixture
+def plant():
+    class Plant:
+        def __init__(self, name):
+            self.name = name
+
+        def create_trip_data(self, trip, tipo_carga, tipo_caja, tipo_turno):
+            return {
+                "trip": trip,
+                "planta": self.name,
+                "tipoCarga": tipo_carga,
+                "tipoCaja": tipo_caja,
+                "areaCarga": self.area_carga,
+                "turno": "2",
+                "tipoTurno": tipo_turno,
+                "carrier": "Carrier 2",
+                "driver": "Driver 2",
+            }
+
+        def get_staging_event_data(self, screen_line, sensores):
+            {
+                "factoryPlant": self.name,
+                "screenLine": screen_line,
+                "sensors": [{"number": i + 1, "active": 0} for i in range(sensores)]
+            }
+
+    return Plant
+
 def test_addTrip_Planning():
     url = os.getenv("API_tripPlanning")
-    data = { 
+    data = {
         "trip": 31,
-        "planta": "PTA 1 (CD)", 
+        "planta": "PTA 1 (CD)",
         "fecha": "2024-11-11"
     }
     post_request(url, data)
-    
-    
-class Plant:
-    def __init__(self, name, area_carga="EXPLANADA/BODEGA"):
-        self.name = name
-        self.area_carga = area_carga
 
-    def create_trip_data(self, trip, tipo_carga, tipo_caja, tipo_turno):
-        return {
-            "trip": trip,
-            "planta": self.name,
-            "tipoCarga": tipo_carga,
-            "tipoCaja": tipo_caja,
-            "areaCarga": self.area_carga,
-            "turno": "2",
-            "tipoTurno": tipo_turno,
-            "carrier": "Carrier 2",
-            "driver": "Driver 2",
-        }
-
-@pytest.mark.parametrize("plant", [
-    Plant("PTA 1 (CD)"),
-    Plant("Plant 4"),
-    Plant("PTA 5 (CD)")
+@pytest.mark.parametrize("plant_instance", [
+    ("PTA 1 (CD)", 12), # 12 sensores
+    ("Plant 4", 7), # 7 sensores
+    ("PTA 5 (CD)", 15)  # 15 sensores
 ])
-@pytest.mark.parametrize("tipoCarga", ["Carga regular", "Carga lateral"])   
+@pytest.mark.parametrize("tipoCarga", ["Carga regular", "Carga lateral"])
 @pytest.mark.parametrize("tipoCaja", ["Caja seca", "Plataforma"])
 @pytest.mark.parametrize("tipoTurno", ["Exportación", "Grupo Comercial", "Nacional", "Recibo mercancía"])
 
-def test_addTrip(plant, tipoCarga, tipoCaja, tipoTurno):
-    url = os.getenv("API_addTrip")
-    data = plant.create_trip_data(update_trip(), tipo_carga=tipoCarga, tipo_caja=tipoCaja, tipo_turno=tipoTurno)
+def test_addTrip(plant, plant_instance, tipoCarga, tipoCaja, tipoTurno):
+    plant_name, sensores = plant_instance
+    plant_obj = plant(plant_name, plant_instance[1])
+    data = plant_obj.create_trip_data(update_trip(), tipo_carga=tipoCarga, tipo_caja=tipoCaja, tipo_turno=tipoTurno)
     post_request(url, data)
     expected_plants = ["PTA 1 (CD)", "Plant 4", "PTA 5 (CD)"]
     assert data["planta"] in expected_plants, f"Unexpected value: {data['planta']}"
 
-
-#Post stagingEvent
-def test_stagingEvent():
+def test_stagingEvent(plant):
+    plant_obj = plant("PTA 1 (CD)")
     url = os.getenv("API_stagingEvent")
-    data = {
-        "factoryPlant": "Pta 1 (CD)",
-        "screenLine": 1,
-        "sensors": [
-           {"number": 1, "active": 0},
-           {"number": 2, "active": 0},
-           {"number": 3, "active": 0},
-           {"number": 4, "active": 0},
-           {"number": 5, "active": 0},
-           {"number": 6, "active": 0},
-           {"number": 7, "active": 0},
-           {"number": 8, "active": 0},
-           {"number": 9, "active": 0},
-           {"number": 10, "active": 0},
-           {"number": 11, "active": 0},
-           {"number": 12, "active": 0},
-           {"number": 13, "active": 0},
-           {"number": 14, "active": 0},
-           {"number": 15, "active": 0}
-        ]
-    }
-    post_request(url, data)
+    data = plant_obj.get_staging_event_data(screen_line=1, sensores=15)
+    try:
+        post_request(url, data) 
+        print(f"Trip sent: {data['factoryPlant']}")
+        time.sleep(10)
+    except KeyboardInterrupt:
+        print("Test interrupted")
+        exit(0)
 
 if __name__ == "__main__":
-    test_addTrip()
-
-    
+    pytest.main()
